cbe7334e53ff6dc8b3178d2dafa373565a4cc43e
Rafael Rios <rvrios22@gmail.com>
2025-05-21 05:54:55 -0700

    add: update functino to event with optomistic updates

--- client/src/components/Event/Event.tsx
+++ client/src/components/Event/Event.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import EventTypes from "../../types/EventTypes";
 import styles from "./Event.module.css";
+import customFetch from "../../utils/customFetch";
 
 function Event({
   id,
@@ -9,7 +11,13 @@ function Event({
   events,
   setEvents,
 }: EventTypes) {
-  
+  const [isEventEditable, setIsEventEditable] = useState<boolean>(false);
+  const [updatedEventData, setUpdatedEventData] = useState({
+    title,
+    description,
+    date,
+  });
+
   const handleDelete = async (id: number) => {
     const options = {
       method: "DELETE",
@@ -28,12 +36,83 @@ function Event({
       console.error(e);
     }
   };
-  return (
-    <section className={styles.section}>
+
+  const handleEdit = async (
+    e: React.FormEvent<HTMLFormElement>,
+    id: number
+  ) => {
+    e.preventDefault();
+    const options: RequestInit = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        // Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+      },
+      body: JSON.stringify(updatedEventData),
+    };
+    try {
+      const data = await customFetch<EventTypes>(`/api/event/${id}`, options);
+      const updatedEvents = events?.map((event) =>
+        event.id === id ? { ...event, ...data } : event
+      );
+      if (updatedEvents && setEvents) {
+        setEvents(updatedEvents);
+      }
+      setIsEventEditable(false);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const readJSX = (
+    <>
       <h2 className="sub-header">{title}</h2>
       <p className="general-text">{description}</p>
       <p className="general-text">{date}</p>
       <button onClick={() => handleDelete(id)}>Delete</button>
+      <button onClick={() => setIsEventEditable(!isEventEditable)}>Edit</button>
+    </>
+  );
+  const editJSX = (
+    <form onSubmit={(e) => handleEdit(e, id)}>
+      <input
+        type="text"
+        name="title"
+        id="title"
+        value={updatedEventData.title}
+        onChange={(e) =>
+          setUpdatedEventData({ ...updatedEventData, title: e.target.value })
+        }
+      />
+      <input
+        type="date"
+        name="date"
+        id="date"
+        value={updatedEventData.date}
+        onChange={(e) =>
+          setUpdatedEventData({ ...updatedEventData, date: e.target.value })
+        }
+      />
+      <textarea
+        name="description"
+        id="description"
+        value={updatedEventData.description}
+        onChange={(e) =>
+          setUpdatedEventData({
+            ...updatedEventData,
+            description: e.target.value,
+          })
+        }
+      ></textarea>
+      <input type="submit" value="Edit" className="button" />
+      <button onClick={() => setIsEventEditable(!isEventEditable)}>
+        Cancel
+      </button>
+    </form>
+  );
+  return (
+    <section className={styles.section}>
+      {isEventEditable ? editJSX : readJSX}
     </section>
   );
 }
