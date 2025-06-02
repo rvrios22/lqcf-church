2e8cdf27ecfe05f0d40b8a1e51acbec5fae2bf6d
Rafael Rios <rvrios22@gmail.com>
2025-06-02 06:34:50 -0700

    remove: unused import

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -6,7 +6,6 @@ import Navbar from "../components/Navbar/Navbar";
 import MobileNavbar from "../components/MobileNavbar/MobileNavbar";
 import Footer from "../components/Footer/Footer";
 import { UserProvider } from "../context/UserContext";
-import { useEffect } from "react";
 
 export const Route = createRootRoute({
   component: RootComponent,
