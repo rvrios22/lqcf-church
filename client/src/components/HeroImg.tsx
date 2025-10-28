537580e7d4ee28790be606d7dd180280967ba84f
Rafael Rios <rvrios22@gmail.com>
2025-10-28 05:22:40 -0700

    fix: hero image sizing error

--- client/src/components/HeroImg.tsx
+++ client/src/components/HeroImg.tsx
@@ -10,7 +10,7 @@ function HeroImg({ name, text }: HeroImgTypes) {
   const [loaded, setLoaded] = useState<boolean>(false);
 
   return (
-    <div className="relative m-auto mt-6 mb-6 flex w-4/5 justify-center">
+    <div className="relative mx-auto my-6 flex w-4/5 justify-center overflow-clip">
       <Image
         srcSet={`
     /api/static/imgs/${name}/${name}-300.webp 300w,
@@ -27,13 +27,13 @@ function HeroImg({ name, text }: HeroImgTypes) {
     (max-width: 320px) 80vw,
     80vw
   "
-        className="m-auto h-[150px] object-cover lg:h-[50vh] lg:object-fill"
+        className="m-auto h-[350px] object-cover lg:h-auto"
         src={`api/static/imgs/${name}/${name}-1000.webp`}
         alt={`${name} header`}
         onLoad={() => setLoaded(true)}
       />
       <h1
-        className={`absolute left-1/2 z-11 -translate-x-1/2 -translate-y-1/2 text-center font-[Inter,'Tahoma','Verdana',sans-serif] text-white transition-all duration-1000 ease-in-out [text-shadow:3px_2px_3px_rgb(0,0,0)] md:text-2xl lg:text-5xl ${loaded ? "top-1/2 opacity-100" : "top-[60%] opacity-0"} `}
+        className={`absolute left-1/2 z-11 -translate-x-1/2 -translate-y-1/2 text-center font-[Inter,'Tahoma','Verdana',sans-serif] text-xl text-white transition-all duration-1000 ease-in-out [text-shadow:3px_2px_3px_rgb(0,0,0)] md:text-2xl lg:text-5xl ${loaded ? "top-1/2 opacity-100" : "top-[60%] opacity-0"} `}
       >
         {text}
       </h1>
