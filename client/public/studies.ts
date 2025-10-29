a01e3696d9a3fe710ad2cda05fe8441ba782b83b
Rafael Rios <rvrios22@gmail.com>
2025-10-29 06:19:32 -0700

    update studies.ts

--- client/public/studies.ts
+++ client/public/studies.ts
@@ -1,16 +1,16 @@
 const studies = [
-    {
-        study: "Sunday Service",
-        desc: "In our Sunday morning service, Pastor Mark has been preaching through the book of Genesis, focusing on chapters 37-50. James Vernon McGee describes this section as focusing on Joseph, who is portrayed as a figure of honor and virtue. McGee notes that Joseph’s life parallels that of Christ in many ways, though he is not explicitly called a type of Christ in the New Testament. This study will explore the life and character of Joseph and the lessons he exemplifies for believers. Join us as we glorify the Lord together."
-    },
-    {
-        study: "Wednesday Service",
-        desc: "The Book of Romans, written by the Apostle Paul, is the focus of our Wednesday service. Known as 'The chief part of the New Testament and the very purest gospel,' this book has profoundly impacted many, including the church father Augustine. The study aims to deepen our faith and understanding of Jesus Christ, who was delivered over to death for our sins and raised to life for our justification. Come grow in your knowledge and relationship with the Lord."
-    },
-    {
-        study: "Sunday School with Pastor Curtis",
-        desc: "Pastor Curtis is leading a study on Church History, exploring Creeds, Catechisms, Councils, and Confessions, starting from Genesis 4:26, where people began calling upon the name of the LORD. The study will cover early essential church councils, from the Council of Jerusalem to the Synod of Dort, key Christian apologists like Athanasius and Augustine, and significant creeds and catechisms, including the Shema, the Athanasian Creed, and the Nicene Creed. The study will also cover Protestant catechisms and confessions, like the Heidelberg Catechism and the Westminster Confession of Faith, and the Protestant Reformation, focusing on reformers like Jon Hus, John Wycliffe, William Tyndale, and Martin Luther."
-    }
+  {
+    study: "Sunday Service",
+    desc: `In our Sunday morning service, Pastor Mark Collins has been preaching through the Apostle Peter’s first epistle. The title of this series is “Hope in the Midst of Trials.” The way our world is going, we all could use a little hope. R.C. Sproul, in his Expositional Commentary on 1–2 Peter, says, “Imagine what it would be like to receive a letter from someone who was a personal friend of Jesus during His earthly ministry. Beyond that, imagine receiving two letters from such a person. That’s exactly what we have in the New Testament correspondence known as 1 & 2 Peter.” Come join us as we study through this amazing letter to the elect of God.`,
+  },
+  {
+    study: "Wednesday Service",
+    desc: "The Book of Romans, written by the Apostle Paul, is the focus of our Wednesday service. Known as 'The chief part of the New Testament and the very purest gospel,' this book has profoundly impacted many, including the church father Augustine. The study aims to deepen our faith and understanding of Jesus Christ, who was delivered over to death for our sins and raised to life for our justification. Come grow in your knowledge and relationship with the Lord.",
+  },
+  {
+    study: "Sunday School with Pastor Curtis",
+    desc: "Pastor Curtis is leading a study on Church History, exploring Creeds, Catechisms, Councils, and Confessions, starting from Genesis 4:26, where people began calling upon the name of the LORD. The study will cover early essential church councils, from the Council of Jerusalem to the Synod of Dort, key Christian apologists like Athanasius and Augustine, and significant creeds and catechisms, including the Shema, the Athanasian Creed, and the Nicene Creed. The study will also cover Protestant catechisms and confessions, like the Heidelberg Catechism and the Westminster Confession of Faith, and the Protestant Reformation, focusing on reformers like Jon Hus, John Wycliffe, William Tyndale, and Martin Luther.",
+  },
 ];
 
-export default studies
+export default studies;
