91f3d512da1482f21c9f041b0c89063225279175
Rafael Rios <rvrios22@gmail.com>
2025-10-30 07:45:16 -0700

    add: from the pastor's heart section

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -2,14 +2,23 @@ import { createFileRoute } from "@tanstack/react-router";
 import HeroImg from "../components/HeroImg";
 import whatsGoingOnData from "../../public/whatsGoingOnData";
 import WhatsGoingOnSquare from "../components/WhatsGoingOnSquare/WhatsGoingOnSquare";
-import { useObserver } from "../hooks/useObserver";
-import { useRef } from "react";
-
+import customFetch from "../utils/customFetch";
+import { useEffect, useState } from "react";
+import type PastorMessage from "../types/PastorMessage";
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
+  const [message, setMessage] = useState<PastorMessage>();
+  useEffect(() => {
+    const fetchPastorMessage = async () => {
+      console.log("asdfa");
+      const message = await customFetch("/api/pastorMessage");
+      setMessage(message);
+    };
+    fetchPastorMessage();
+  }, []);
   return (
     <>
       <HeroImg name="lqcfHome" text="La Quinta Christian Fellowship Church" />
@@ -28,9 +37,7 @@ function RouteComponent() {
         of His will, to the praise of the glory of His grace, which He freely
         bestowed on us in the Beloved. Ephesians 1:3-6
       </p>
-      <section
-        className="mx-auto w-[90%] px-4 md:grid md:grid-cols-2 md:gap-4 lg:grid-cols-3"
-      >
+      <section className="mx-auto w-[90%] px-4 md:grid md:grid-cols-2 md:gap-4 lg:grid-cols-3">
         {whatsGoingOnData.map(({ title, desc, link, name, alt }, idx) => (
           <WhatsGoingOnSquare
             key={idx}
@@ -41,6 +48,15 @@ function RouteComponent() {
             alt={alt}
           />
         ))}
+      </section>
+      <section className="mb-4">
+        <h3 className="sub-header">From Our Pastors Hearts</h3>
+        {message && (
+          <>
+            <p className="general-text">{message.message}</p>
+            <p className="bold w-[90%] text-right">- {message.author}</p>
+          </>
+        )}
       </section>
     </>
   );
---
+++ client/src/types/PastorMessage.d.ts
@@ -0,0 +1,7 @@
+interface PastorMessage {
+  id: number;
+  author: string;
+  message: string;
+}
+
+export default PastorMessage;
