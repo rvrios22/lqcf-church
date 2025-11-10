e7ad2630119861c4331cdfcafc3b17a796c2f401
Rafael Rios <rvrios22@gmail.com>
2025-11-10 07:16:44 -0800

    pictures and change datatype of monthEvent description model

--- server/src/models/monthEvent.ts
+++ server/src/models/monthEvent.ts
@@ -1,19 +1,26 @@
-import { Sequelize, DataTypes, Model, ModelStatic, } from "sequelize"
+import { Sequelize, DataTypes, Model, ModelStatic } from "sequelize";
 
-module.exports = (sequelize: Sequelize, dataTypes: typeof DataTypes): ModelStatic<Model> => {
-    const MonthEvent = sequelize.define('monthEvent', {
-        title: {
-            type: dataTypes.STRING,
-            allowNull: false
-        },
-        description: {
-            type: dataTypes.TEXT,
-            allowNull: false
-        },
-        date: {
-            type: dataTypes.DATE,
-            allowNull: true
-        },
-    }, {})
-    return MonthEvent
-}
+module.exports = (
+  sequelize: Sequelize,
+  dataTypes: typeof DataTypes
+): ModelStatic<Model> => {
+  const MonthEvent = sequelize.define(
+    "monthEvent",
+    {
+      title: {
+        type: dataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: dataTypes.TEXT("long"),
+        allowNull: false,
+      },
+      date: {
+        type: dataTypes.DATE,
+        allowNull: true,
+      },
+    },
+    {}
+  );
+  return MonthEvent;
+};
