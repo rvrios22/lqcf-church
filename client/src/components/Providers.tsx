16750c4c52fd0aaef1f46f87e0b030b6d3ee7bcd
Rafael Rios <rvrios22@gmail.com>
2025-11-20 06:49:52 -0800

    adjust stale time to an hour on QueryClient

--- client/src/components/Providers.tsx
+++ client/src/components/Providers.tsx
@@ -7,7 +7,9 @@ import {
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { UserProvider } from "../context/UserContext";
-export const queryClient = new QueryClient();
+export const queryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: 1000 * 60 * 60 } },
+});
 
 declare module "@react-types/shared" {
   interface RouterConfig {
