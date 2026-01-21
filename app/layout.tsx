375239ab01556cf1e7980de3441f8b3de0c8dec5
Rafael Rios <rvrios22@gmail.com>
2026-01-21 06:56:45 -0800

    add custom utlity classes and default font styles to body

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${firaSans.variable} ${greatVibes.variable} ${inter.variable} antialiased`}
+        className={`${firaSans.variable} ${greatVibes.variable} ${inter.variable} antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,5 @@ import { Button } from "@heroui/react";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Button>Test</Button>
-       </div>
-  );
+  return <div></div>;
 }
